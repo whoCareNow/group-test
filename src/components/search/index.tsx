52ab257834a3935752d161bf9ae05aa4f621895a
shen <xiaohuan.shen@hand-china.com>
2022-07-29 20:56:04 +0800

    🌈 style: 没什么

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,26 +1,40 @@
-import { Input } from "antd";
-import { useEffect, useState } from "react";
+import { Input, Radio, RadioChangeEvent, Select } from "antd";
+import { useEffect, useMemo, useState } from "react";
 import Fetch from "../../api/fetch";
 import { SearchEvent } from "../../type/Event";
-import _ from "lodash";
+import _, { iteratee } from "lodash";
 import { userInfo } from "../../type/Data";
 import List from "./List";
+import Item from "antd/lib/list/Item";
 const init: () => Promise<userInfo[]> = async () => {
   const res = await Fetch.get("getuser");
   if (res.message === "success") return res.data;
   else return [];
 };
 const getuser = _.throttle(init, 30000, { leading: true, trailing: false });
+const typeForQuery: Array<{ type: string; cn: string }> = [
+  { type: "name", cn: "姓名" },
+  { type: "city", cn: "城市" },
+];
+
 export default function Search() {
   const [userList, setUserList] = useState<userInfo[]>([]);
   const [query, setQuery] = useState<string>("");
+  const [queryType, setQueryType] = useState<string>("name");
   const onSearch = (value: string, e?: SearchEvent) => {
     setQuery(value);
   };
-  const searchUserName: () => userInfo[] = () => {
-    if (query === "") return userList;
-    else return userList.filter((user) => user.name.indexOf(query) !== -1);
+  const handleChange = (e: RadioChangeEvent) => {
+    setQueryType(e.target.value);
   };
+  const searchUserName = useMemo<userInfo[]>(() => {
+    if (query === "") return userList;
+    else if (queryType === "name")
+      return userList.filter((user) => user.name.indexOf(query) !== -1);
+    else if (queryType === "city")
+      return userList.filter((user) => user.city.indexOf(query) !== -1);
+    else return userList;
+  }, [userList, query, queryType]);
   console.log("search render");
   useEffect(() => {
     console.log("search mount");
@@ -45,8 +59,22 @@ export default function Search() {
           style={{ width: 200 }}
         />
       </div>
+      <div className="selection-wrapper">
+        <Radio.Group
+          value={queryType}
+          defaultValue="name"
+          style={{ width: 200 }}
+          onChange={handleChange}
+        >
+          {typeForQuery.map((item) => (
+            <Radio value={item.type} key={item.type}>
+              {item.cn}
+            </Radio>
+          ))}
+        </Radio.Group>
+      </div>
       <div className="lists-wrapper">
-        {searchUserName().map((user) => (
+        {searchUserName.map((user) => (
           <List
             key={user.id}
             id={user.id}
